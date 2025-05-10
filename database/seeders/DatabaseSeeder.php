91e898523017ca84bbc89511cce43463237cf2e7
Juan Ballesteros <juancal1398@gmail.com>
2025-05-10 12:26:52 -0500

    add test user to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,10 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@admin.com',
+            'password' => '1234sangel',
         ]);
     }
 }
