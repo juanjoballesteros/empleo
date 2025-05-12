9982700db75028da70ddd214f8643765c4a20f89
Juan Ballesteros <juancal1398@gmail.com>
2025-05-12 12:17:52 -0500

    fix: welcome view deleted

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::redirect('/', 'login')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
