7f3efdf0b8babf1ff45aa80b819938f57f1834e7
Juan Ballesteros <juancal1398@gmail.com>
2025-06-18 10:55:01 -0500

    feat: add example cv for test candidate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,10 +5,19 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Enums\Roles;
+use App\Models\BasicEducationInfo;
+use App\Models\BirthInfo;
 use App\Models\Candidate;
 use App\Models\Company;
+use App\Models\ContactInfo;
+use App\Models\Cv;
+use App\Models\HigherEducation;
+use App\Models\LanguageInfo;
+use App\Models\PersonalInfo;
+use App\Models\ResidenceInfo;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\WorkExperience;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
@@ -25,7 +34,6 @@ final class DatabaseSeeder extends Seeder
                 'email' => 'admin@admin.com',
                 'password' => '1234sangel',
             ]);
-
         $user->assignRole(Roles::EMPRESA);
 
         $candidate = User::factory()
@@ -35,7 +43,19 @@ final class DatabaseSeeder extends Seeder
                 'email' => 'juan@juan.com',
                 'password' => '1234sangel',
             ]);
-
         $candidate->assignRole(Roles::CANDIDATO);
+
+        Cv::factory()
+            ->for($candidate)
+            ->for($candidate->userable)
+            ->has(PersonalInfo::factory())
+            ->has(BirthInfo::factory())
+            ->has(ContactInfo::factory())
+            ->has(ResidenceInfo::factory())
+            ->has(BasicEducationInfo::factory())
+            ->has(HigherEducation::factory(2))
+            ->has(WorkExperience::factory(2))
+            ->has(LanguageInfo::factory(2))
+            ->create();
     }
 }
