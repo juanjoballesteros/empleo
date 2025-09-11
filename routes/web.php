60aa0330b63283131b7d89b29141f66ebbc05e82
Juan Ballesteros <juancal1398@gmail.com>
2025-09-11 17:41:49 -0500

    si

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::view('/', 'home')->name('home');
 Route::get('cv/import', Cv\Import::class)->name('cv.import');
 
 Route::get('whatsapp', [WhatsAppController::class, 'token'])->name('whatsapp.get');
-// Route::post('whatsapp', [App\Http\Controllers\WhatsAppController::class, 'token'])->name('whatsapp.post');
+Route::post('whatsapp', [WhatsAppController::class, 'webhook'])->name('whatsapp.webhook');
 
 Route::middleware(['auth', 'type'])->group(function () {
     Route::middleware('cv')->group(function () {
